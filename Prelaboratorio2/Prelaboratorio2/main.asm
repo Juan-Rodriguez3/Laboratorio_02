d0007e10abb6ec57dd70cdcd218bb47a27fa9a57
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-02-14 16:06:16 -0600

    Cambios en las salidas
    Declaramos los puertos C y D como salidas. y cambiamos la función del código para encender una led cada 100 ms

--- Prelaboratorio2/Prelaboratorio2/main.asm
+++ Prelaboratorio2/Prelaboratorio2/main.asm
@@ -1,76 +1,87 @@
-/*
-* EjemploTimer0.asm
-*
-* Creado: 04-Feb-25 10:25:52 AM
-* Autor : Pedro Castillo
-* Descripci n:?
-*/
-/**************/
-// Encabezado (Definici n de Registros, Variables y Constantes)?
-.include "M328PDEF.inc" // Include definitions specific to ATMega328P
+;*********************
+; Universidad del Valle de Guatemala
+; IE2025: Programacion de Microcontroladores
+;
+; Author: Juan Rodriguez
+; Proyecto: Prelab II
+; Hardware: ATmega328P
+; Creado: 14/02/2025
+; Modificado: 14/02/2025
+; Descripcion: Este programa consiste en un contador binario de 4 bits que incrementa cada 100 ms. 
+;*********************
+.include "M328PDEF.inc"
+.def COUNTER = R20
 .cseg
 .org 0x0000
-.def COUNTER = R20
-/**************/
-	// Configuraci n de la pila?
-	LDI		R16, LOW(RAMEND)
-	OUT		SPL, R16
-	LDI		R16, HIGH(RAMEND)
-	OUT		SPH, R16
-	/**************/
-	// Configuracion MCU
-	SETUP:
-	// Configurar Prescaler "Principal"
+
+//Configuraci?n de pila //0x08FF
+	LDI		R16, LOW(RAMEND)	// Cargar 0xFF a R16
+	OUT		SPL, R16			// Cargar 0xFF a SPL
+	LDI		R16, HIGH(RAMEND)	
+	OUT		SPH, R16			// Cargar 0x08 a SPH
+
+SETUP:
+	// Configurar Prescaler 
 	LDI		R16, (1 << CLKPCE)
-	STS		CLKPR, R16	// Habilitar cambio de PRESCALER
+	STS		CLKPR, R16 // Habilitar cambio de PRESCALER
 	LDI		R16, 0b00000100
-	STS		CLKPR, R16	// Configurar Prescaler a 16 F_cpu = 1MHz
+	STS		CLKPR, R16 // Configurar Prescaler en 1MHz
+	
 	// Inicializar timer0
-	CALL	INIT_TMR0
-	// Configurar PB5 como salida para usarlo como "LED"
-	SBI		DDRB, 5			// Establecer bit PB5 como salida
-	SBI		DDRB, 0
-	CBI		PORTB, 5		// Obligar a LED a estar "APAGADO" inicialmente
-	CBI		PORTB, 0
-	// Deshabilitar serial (esto apaga los dem s LEDs del Arduino)?
+	CALL    INIT_TMR0
+
+	  //Configurar los pines como entradas y salidas
+
+	  //Puerto C como entrada
+	LDI		R16, 0x00
+	OUT		DDRC, R16
+	LDI		R16, 0xFF
+	OUT		PORTC, R16		;Pull-up
+
+	//Puerto B como salida
+	LDI		R16, 0xFF
+	OUT		DDRB, R16 // Puerto B como salida
+	LDI		R16, 0x00
+	OUT		PORTB, R16 //El puerto B conduce cero logico.
+
+	//Puerto D como salida
+	LDI		R16, 0xFF
+	OUT		DDRD, R16 // Puerto D como salida
+	LDI		R16, 0x00
+	OUT		PORTD, R16 //El puerto D conduce cero logico.
+
+	// Deshabilitar serial 
 	LDI		R16, 0x00
 	STS		UCSR0B, R16
 	LDI		COUNTER, 0x00
-	/**************/
-	// Loop Infinito
-	MAIN_LOOP:
-	IN		R16, TIFR0		// Leer registro de interrupci n de TIMER 0?
-	SBRS	R16, TOV0		// Salta si el bit 0 est "set" (TOV0 bit)?
-	RJMP	MAIN_LOOP		// Reiniciar loop
-	SBI	 TIFR0, TOV0		// Limpiar bandera de "overflow"
+	LDI		R17, 0x00
+
+	MAIN:
+	IN		R16, TIFR0 // Leer registro de interrupcion 
+	SBRS	R16, TOV0 // Salta si el bit 0 esta en 1, es la bandera de of
+	RJMP	MAIN // Reiniciar loop si no hay of
+	SBI		TIFR0, TOV0 // Limpiar bandera de "overflow"
 	LDI		R16, 100
-	OUT		TCNT0, R16		// Volver a cargar valor inicial en TCNT0
+	OUT		TCNT0, R16 // Volver a cargar valor inicial en TCNT0
 	INC		COUNTER
-	CPI		COUNTER, 50		// R20 = 50 after 500ms (since TCNT0 is set to
-	10 ms)
-	BRNE MAIN_LOOP
-	CLR COUNTER
-	SBI PINB, PB5
-	SBI PINB, PB0
-	RJMP MAIN_LOOP
-	/**************/
-	// NON-Interrupt subroutines
-	INIT_TMR0:
+	CPI		COUNTER, 10 // R20 = 10 después 100ms (el TCNT0 está en to 10 ms)
+	BRNE	MAIN
+	CLR		COUNTER
+	INC     R17
+	CPI		R17, 0x10 //comparar con 16
+	BREQ	overflow //si es 16 ejecutar salta a overflow
+	OUT		PORTB, R17 //si no es 16 cargar el valor 
+	RJMP	MAIN
+
+	overflow:
+	LDI		R17, 0x00 //si es 16 el valor hay of y se hace 0
+	OUT		PORTB, R17
+	RJMP	MAIN
+
+// NON-Interrupt subroutines
+INIT_TMR0:
 	LDI		R16, (1<<CS01) | (1<<CS00)
-	OUT		TCCR0B, R16		// Setear prescaler del TIMER 0 a 64
+	OUT		TCCR0B, R16 // Setear prescaler del TIMER 0 a 64
 	LDI		R16, 100
-	OUT		TCNT0, R16		// Cargar valor inicial en TCNT0
+	OUT		TCNT0, R16 // Cargar valor inicial en TCNT0
 	RET
-	/**************/
-	// Interrupt routines
-	/*************//*************/
-	// NON-Interrupt subroutines
-	INIT_TMR0:
-	LDI		R16, (1<<CS01) | (1<<CS00)
-	OUT		TCCR0B, R16		// Setear prescaler del TIMER 0 a 64
-	LDI		R16, 100
-	OUT		TCNT0, R16		// Cargar valor inicial en TCNT0
-	RET
-	/**************/
-	// Interrupt routines
-	/**************
